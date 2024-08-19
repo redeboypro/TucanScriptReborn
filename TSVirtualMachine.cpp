0c34f65b80f49a9fb5d928964906339de0bda7aa
Sasha Kotya <73701605+redeboypro@users.noreply.github.com>
2024-08-19 20:24:33 +0300

    Virtual Machine UPD#1

--- TSVirtualMachine.cpp
+++ TSVirtualMachine.cpp
@@ -1,23 +1,95 @@
 #include "TSVirtualMachine.h"
 
 TSStack::TSStack (ULONG_PTR size) : m_End(NULL) {
-	m_Data = new TSEntity[size];
+	m_Data = new TSValue[size];
 }
 
 TSStack::~TSStack () {
 	delete[] m_Data;
 }
 
-void TSStack::Push (TSEntity entity) {
+void TSStack::Push (TSValue entity) {
 	m_Data[m_End] = entity;
 	m_End++;
 }
 
-TSEntity TSStack::Pop () {
+TSValue TSStack::Pop () {
 	m_End--;
 	return m_Data[m_End];
 }
 
-TSVirtualMachine::TSVirtualMachine (ULONG_PTR stackSize) : m_Stack(stackSize) {
+void TSManagedMemory::Free () {
+	if (m_Size > 1) { 
+		delete[] m_Memory; 
+	}
+	else {
+		delete m_Memory; 
+	}
+}
+
+TSVirtualMachine::TSVirtualMachine (ULONG_PTR stackSize, TSASM asmSet) : 
+	m_Stack(stackSize), m_ASM(std::move(asmSet)) {}
+
+TSVirtualMachine::~TSVirtualMachine () {
+	m_Allocator.FreeRoot ();
+	delete[] m_ASM.m_Instructions;
+}
+
+TSAllocator::TSAllocator () : m_Begin (nullptr), m_End (nullptr) {}
+
+TSManagedMemory* TSAllocator::Alloc (TSValue* memory, ULONG_PTR size) {
+	auto* allocated = new TSManagedMemory {
+		.m_Memory = memory,
+		.m_Size = size,
+		.m_Next = nullptr,
+		.m_Previous = m_End
+	};
+
+	if (m_End) {
+		m_End->m_Next = allocated;
+	}
+
+	m_End = allocated;
+
+	if (!m_Begin) {
+		m_Begin = allocated;
+	}
+
+	return allocated;
+}
+
+void TSAllocator::Free (TSManagedMemory* ptr) {
+	auto* previousPtr = ptr->m_Previous;
+	auto* nextPtr = ptr->m_Next;
+
+	ptr->Free ();
+
+	if (previousPtr) {
+		previousPtr->m_Next = nextPtr;
+	}
+	else {
+		m_Begin = nextPtr;
+	}
+
+	if (nextPtr) {
+		nextPtr->m_Previous = previousPtr;
+	} 
+	else {
+		m_End = previousPtr;
+	}
+
+	delete ptr;
+}
+
+void TSAllocator::FreeRoot () {
+
+	while (m_Begin) {
+		auto* nextPtr = m_Begin->m_Next;
+		delete m_Begin;
+		m_Begin = nextPtr;
+	}
+}
+
+void TSVirtualMachine::Run () {
 
 }
--- TSVirtualMachine.h
+++ TSVirtualMachine.h
@@ -2,7 +2,41 @@
 #define VIRTUALMACHINE_H
 
 #include <Windows.h>
+#include <iostream>
 #include <vector>
+
+enum TSOperation : CHAR {
+	PUSH,
+	JMP,
+
+	MEMALLOC,
+	MEMDEALLOC,
+	MEMSET,
+	MEMCPY,
+
+	TOC,
+	TOUC,
+	TOU16,
+	TOU32,
+	TOU64,
+	TOI16,
+	TOI32,
+	TOI64,
+	TOF32,
+	TOF64,
+
+	ADD,
+	SUB,
+	MUL,
+	DIV,
+
+	CMPE,
+	CMPNE,
+	CMPG,
+	CMPL,
+	CMPGE,
+	CMPLE,
+};
 
 enum TSDataType : CHAR {
 	CHAR_T,
@@ -15,48 +49,82 @@ enum TSDataType : CHAR {
 	INT64_T,
 	FLOAT32_T,
 	FLOAT64_T,
-	PTR_T
+	MANAGED_T,
+	MANAGEDARR_T,
+	NATIVEPTR_T
 };
 
 union TSData {
-	CHAR      m_C;
-	BYTE      m_UC;
-	UINT16    m_U16;
-	UINT32    m_U32;
-	UINT64    m_U64;
-	INT16     m_I16;
-	INT32     m_I32;
-	INT64     m_I64;
-	FLOAT     m_F32;
-	DOUBLE    m_F64;
-	ULONG_PTR m_Ptr;
+	CHAR             m_C;
+	BYTE             m_UC;
+	UINT16           m_U16;
+	UINT32           m_U32;
+	UINT64           m_U64;
+	INT16            m_I16;
+	INT32            m_I32;
+	INT64            m_I64;
+	FLOAT            m_F32;
+	DOUBLE           m_F64;
+	TSManagedMemory* m_ManagedPtr;
+	VOID*            m_NativePtr;
 };
 
-struct TSEntity {
+struct TSValue {
 	TSDataType m_Type;
 	TSData     m_Data;
 };
 
-struct TSBuffer {
-	VOID* m_Memory;
+struct TSManagedMemory {
+	TSValue*         m_Memory;
+	ULONG_PTR        m_Size;
+	INT              m_NumBindings;
+	TSManagedMemory* m_Next;
+	TSManagedMemory* m_Previous;
+
+	void Free ();
+};
+
+struct TSInstruction {
+	TSOperation m_Operation;
+	TSValue m_Value;
+};
+
+struct TSASM {
+	TSInstruction* m_Instructions;
+	ULONG_PTR      m_Size;
 };
 
 class TSStack {
-	TSEntity* m_Data;
-	INT       m_End;
+	TSValue* m_Data;
+	INT      m_End;
 public:
 	TSStack (ULONG_PTR size);
 	~TSStack ();
 
-	void Push (TSEntity entity);
-	TSEntity Pop ();
+	void Push (TSValue entity);
+	TSValue Pop ();
+};
+
+class TSAllocator {
+	TSManagedMemory* m_Begin;
+	TSManagedMemory* m_End;
+public:
+	TSAllocator ();
+
+	TSManagedMemory* Alloc (TSValue* memory, ULONG_PTR size);
+	void Free (TSManagedMemory* ptr);
+	void FreeRoot ();
 };
 
 class TSVirtualMachine {
-	TSStack               m_Stack;
-	std::vector<TSBuffer> m_Heap;
+	TSStack     m_Stack;
+	TSAllocator m_Allocator;
+	TSASM       m_ASM;
 public:
-	TSVirtualMachine (ULONG_PTR stackSize);
+	TSVirtualMachine (ULONG_PTR stackSize, TSASM asmSet);
+	~TSVirtualMachine ();
+
+	void Run ();
 };
 
 #endif
